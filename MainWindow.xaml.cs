3f86aff037e09e43feb9b9006a76e8b6aa64554e
Raphael <raphaelrodriguesssilva@gmail.com>
2023-07-05 22:09:58 -0300

    Versao 1.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,6 +28,11 @@ namespace PlayerWPF
         {
             InitializeComponent();
 
+            MessageBox.Show("Bem-vindo ao Media Player!\n\nPara usar o player, siga as seguintes instruções:\n\n1. Clique no botão 'Selecionar Mídia' para escolher um arquivo de mídia.\n2. Clique no botão 'Play' para reproduzir a mídia selecionada.\n3. Clique no botão 'Pause' para pausar a reprodução da mídia.\n4. Clique no botão 'Parar' para interromper a reprodução da mídia.\n\nAproveite a experiência!",
+                        "Instruções",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+
             MediaPlayerModel = new MediaPlayerModel();
             DataContext = MediaPlayerModel;
         }
@@ -46,35 +51,79 @@ namespace PlayerWPF
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            if (MediaPlayerModel.IsPlaying)
+            try
             {
-                // Retomar a reprodução a partir da posição congelada
-                mediaElement.Position = currentPosition;
-                mediaElement.Play();
-            }
-            else
-            {
-                // Iniciar a reprodução do início
-                mediaElement.Play();
-            }
+                if (MediaPlayerModel.IsPlaying)
+                {
+                    // Retomar a reprodução a partir da posição congelada
+                    mediaElement.Position = currentPosition;
+                    mediaElement.Play();
+                }
+                else
+                {
+                    // Iniciar a reprodução do início
+                    if (string.IsNullOrEmpty(MediaPlayerModel.MediaPath))
+                    {
+                        OpenFileDialog openFileDialog = new OpenFileDialog();
+                        openFileDialog.Filter = "Arquivos de Mídia|*.mp3;*.mp4;*.avi";
 
-            MediaPlayerModel.IsPlaying = true;
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            string selectedFilePath = openFileDialog.FileName;
+                            MediaPlayerModel.MediaPath = selectedFilePath;
+                        }
+                        else
+                        {
+                            return; // Se o usuário cancelar a seleção de mídia, retornar sem iniciar a reprodução
+                        }
+                    }
+
+                    mediaElement.Play();
+                }
+
+                MediaPlayerModel.IsPlaying = true;
+            }
+            catch (Exception ex)
+            {
+                // Lidar com exceções, se necessário
+                //MessageBox.Show($"Erro ao reproduzir o vídeo: {ex.Message}");
+            }
         }
+
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-            mediaElement.Pause();
-            currentPosition = mediaElement.Position;
-            MediaPlayerModel.IsPlaying = false;
+            try
+            {
+                if (mediaElement.Source != null && mediaElement.CanPause)
+                {
+                    mediaElement.Pause();
+                    currentPosition = mediaElement.Position;
+                    MediaPlayerModel.IsPlaying = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Lidar com exceções, se necessário
+                MessageBox.Show($"Erro ao pausar o vídeo: {ex.Message}");
+            }
         }
+
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            mediaElement.Stop();
-            MediaPlayerModel.MediaPath = null;
-            MediaPlayerModel.IsPlaying = false;
+            try
+            {
+                mediaElement.Stop();
+                MediaPlayerModel.MediaPath = null;
+                MediaPlayerModel.IsPlaying = false;
+            }
+            catch (Exception ex)
+            {
+                // Lidar com exceções, se necessário
+                MessageBox.Show($"Erro ao parar o vídeo: {ex.Message}");
+            }
         }
+
     }
 }
-//Quando pausa ou Stopa o vídeo está fechando o programa.
-//Tem que tirar o IsPlaying = false;
